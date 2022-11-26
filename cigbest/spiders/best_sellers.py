97ee8dd0f80c0638372347fc9c3f94a6bb4e2b45
decodesaurav <decodesaurav@gmail.com>
2022-11-26 20:56:09 +0545

    working currently on multipage webscraping

--- cigbest/spiders/best_sellers.py
+++ cigbest/spiders/best_sellers.py
@@ -4,8 +4,14 @@ import scrapy
 
 class BestSellersSpider(scrapy.Spider):
     name = 'best_sellers'
-    allowed_domains = ['www.cigbest.com/collections/best-sellers']
-    start_urls = ['http://www.cigbest.com/collections/best-sellers/']
+    allowed_domains = ['www.cigbest.com']
+    start_urls = ['https://www.cigbest.com/collections/best-sellers']
 
     def parse(self, response):
-        pass
+        for product in response.xpath("//div[@class='product-list-item-list']/div/div[@class='col product-item-list']"):
+            yield{
+                'title': product.xpath(".//div[@class='product-item__wrapper shopline-element-product-item']/div/a/@data-name").get(),
+                'url': response.urljoin(product.xpath(".//div[@class='product-item__wrapper shopline-element-product-item']/div/a/@href").get()),
+                'discounted_price': product.xpath(".//div[@class='product-item__wrapper shopline-element-product-item']/div/a/@data-price").get(),
+                'original_price': product.xpath(".//div[@class='product-item-price']/div/span[3]/@data-product-item-price").get()
+            }

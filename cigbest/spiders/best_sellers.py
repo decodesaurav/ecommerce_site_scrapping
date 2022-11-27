fad8004c2b4ce5ccbb363478557ba3ffaf0d214d
decodesaurav <decodesaurav@gmail.com>
2022-11-27 17:47:35 +0545

    multipage pagination with spoof header

--- cigbest/spiders/best_sellers.py
+++ cigbest/spiders/best_sellers.py
@@ -5,7 +5,11 @@ import scrapy
 class BestSellersSpider(scrapy.Spider):
     name = 'best_sellers'
     allowed_domains = ['www.cigbest.com']
-    start_urls = ['https://www.cigbest.com/collections/best-sellers']
+
+    def start_requests(self):
+        yield scrapy.Request(url='https://www.cigbest.com/collections/best-sellers', callback=self.parse, headers={
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36'
+        })
 
     def parse(self, response):
         for product in response.xpath("//div[@class='product-list-item-list']/div/div[@class='col product-item-list']"):
@@ -15,3 +19,11 @@ class BestSellersSpider(scrapy.Spider):
                 'discounted_price': product.xpath(".//div[@class='product-item__wrapper shopline-element-product-item']/div/a/@data-price").get(),
                 'original_price': product.xpath(".//div[@class='product-item-price']/div/span[3]/@data-product-item-price").get()
             }
+
+        next_page = response.urljoin(response.xpath(
+            "//span[@class='pagination_next pagination_item']/a/@href").get())
+
+        if next_page:
+            yield scrapy.Request(url=next_page, callback=self.parse, headers={
+                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36'
+            })
